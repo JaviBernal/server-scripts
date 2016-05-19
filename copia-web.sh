a7714229402ff1a994d255f4da1432776a4dba9e
JaviBernal <github@cualquiera.net>
2016-05-19 12:47:05 +0200

    Cambios estéticos

--- copia-web.sh
+++ copia-web.sh
@@ -6,18 +6,19 @@
 WEBROOT='/var/www/htdocs'
 DIRCOPIAS='/root/copias-web'
 FICHNAME=$1-`date +%y%m%d`-`date +%R`
-MYPASS='pass'
+MYPASS='ucnQZFVPH6r*A'
 
 ## Colores
-AZUL='\e[0;34m'
-VERDE='\e[0;32m'
-RESETCOLOR='\e[0m'
+AZUL='\033[0;34m'
+PURPLE='\033[0;35m'
+VERDE='\033[0;32m'
+RESETCOLOR='\033[0m'
 
-echo -e "$VERDE\nComprimiendo web ..."
+echo -e "$VERDE\nComprimiendo web $PURPLE$FICHNAME.tgz $VERDE..."
 tar -C $WEBROOT -czf $DIRCOPIAS/$FICHNAME.tgz $1
-echo -e "Comprimiendo BD ..."
+echo -e "Comprimiendo BD  $PURPLE$FICHNAME-$2.sql.gz $VERDE..."
 mysqldump -p$MYPASS --opt $2 |gzip -c > $DIRCOPIAS/$FICHNAME-$2.sql.gz
 
-echo -e "$AZUL\nResultado de la copia:\n"
+echo -e "$VERDE\nCopias de la web:\n$AZUL"
 ls -lh $DIRCOPIAS/$1*
 echo -e "$RESETCOLOR"
